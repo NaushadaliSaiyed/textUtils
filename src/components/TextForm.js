4214db0e98e8d71c55dd9e4ac1397521a786f5e7
Your Name <127712400+NaushadaliSaiyed@users.noreply.github.com>
2023-06-21 17:23:13 +0530

    naushad agian

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,15 +40,15 @@ export default function TextForm(props) {
     }
 
     const handleCopyClick = () => {
-        const text = document.getElementById("myBox");
-        text.select();
-        if (text.value.length > 0) {
-            navigator.clipboard.writeText(text.value)
-            document.getSelection().removeAllRanges();
+        // const text = document.getElementById("myBox");
+        // text.select();
+        // if (text.value.length > 0) {
+            navigator.clipboard.writeText(text)
+            // document.getSelection().removeAllRanges();
             props.showAlert("Copy to Clipboard!", "success")
-        } else {
-            props.showAlert("please type something!", "warning")
-        }
+        // } else {
+            // props.showAlert("please type something!", "warning")
+        // }
 
     }
 
